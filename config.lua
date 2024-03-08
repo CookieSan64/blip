fdc876cdc87041785f250019c9c8330602f22cf0
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 03:15:30 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,59 @@
+Config = {}
+
+Config.Map = {
+  --Autres
+  {name="Centre Commercial",color=24, id=89,x = -554.4006, y = -611.3129, z = 34.68182},
+  {name="Rental Company",color=2, id=225,x = -1031.052, y = -2734.502, z = 19.16927},
+  {name="Pole Emploi",color=5, id=407,x = -265.02, y = -964.12, z = 31.21},
+  {name="Burger Shot",color=49, id=536,x = -1191.378, y = -891.3909, z = 13.79838},
+  {name="Gouvernement", id=419,x = -542.7756, y = -207.90023, z = 37.6497},
+  
+  --Supérette
+  --[[{name= "Supérette",color=25, id=59,x = 374.5232, y = 326.8159, z = 103.5663}, 
+  {name= "Supérette",color=25, id=59,x = 2556.52, y = 382.657, z = 108.6228}, 
+  {name= "Supérette",color=25, id=59,x = -3040.265, y = 585.973, z = 7.908857}, 
+  {name= "Supérette",color=25, id=59,x = -3242.425, y = 1001.809, z = 12.83063}, 
+  {name= "Supérette",color=25, id=59,x = 547.3649, y = 2670.692, z = 42.15641}, 
+  {name= "Supérette",color=25, id=59,x = 1961.263, y = 3741.588, z = 32.34366}, 
+  {name= "Supérette",color=25, id=59,x = 2678.098, y = 3281.375, z = 55.24106}, 
+  {name= "Supérette",color=25, id=59,x = 1729.793, y = 6415.137, z = 35.03714}, 
+  {name= "Supérette",color=25, id=59,x = 162.2461, y = 6640.776, z = 31.69887}, 
+  {name= "Supérette",color=25, id=59,x = 26.27455, y = -1346.854, z = 29.49695}, 
+  {name= "Supérette",color=25, id=59,x = -1223.014, y = -907.2841, z = 12.32635}, 
+  {name= "Supérette",color=25, id=59,x = 1135.713, y = -981.7485, z = 46.41579}, 
+  {name= "Supérette",color=25, id=59,x = -1487.223, y = -378.9617, z = 40.16339}, 
+  {name= "Supérette",color=25, id=59,x = -2968.243, y = 390.910, z = 15.043}, 
+  {name= "Supérette",color=25, id=59,x = 1166.024, y = 2708.930, z = 38.157}, 
+  {name= "Supérette",color=25, id=59,x = 1392.104, y = 3604.881, z = 34.98093}, 
+  {name= "Supérette",color=25, id=59,x = -552.7488, y = -584.5939, z = 34.68192},
+  {name= "Supérette",color=25, id=59,x = 4467.874, y = -4464.809, z = 4.247197}, 
+  {name= "Supérette",color=25, id=59,x = -160.3747, y = 6322.693, z = 31.58689}, 
+  {name= "Supérette",color=25, id=59,x = 814.2622, y = -782.2833, z = 26.17495}, ]]
+
+  --Ammunation
+  --[[{name= "Ammunation", id=110,x = -660.8758, y = -938.2927, z = 21.82934}, 
+  {name= "Ammunation", id=110,x = 814.9905, y = -2153.534, z = 29.61918}, 
+  {name= "Ammunation", id=110,x = 1695.812, y = 3757.078, z = 34.70532}, 
+  {name= "Ammunation", id=110,x = -328.4692, y = 6081.131, z = 31.45478}, 
+  {name= "Ammunation", id=110,x = 248.975, y = -50.28921, z = 69.94116}, 
+  {name= "Ammunation", id=110,x = 16.35384, y = -1109.121, z = 29.79721}, 
+  {name= "Ammunation", id=110,x = 2566.751, y = 297.3508, z = 108.735}, 
+  {name= "Ammunation", id=110,x = -1114.777, y = 2697.152, z = 18.55425}, 
+  {name= "Ammunation", id=110,x = 842.8907, y = -1030.253, z = 29.19485}, 
+  {name= "Ammunation", id=110,x = -1309.188, y = -392.8411, z = 36.69577},]] 
+
+  --Bars
+  --[[{name= "Bar", id=93,x = 4903.99, y = -4940.117, z = 3.361453},
+  {name= "Bar", id=93,x = 128.2054, y = -1285.299, z = 29.27996},
+  {name= "Bar", id=93,x = -560.119, y = 287.0548, z = 82.17639},]]
+
+  --Pharmacie
+  --[[{name= "Pharmacie",color=77, id=51,x = -506.7789, y = 290.4904, z = 83.38808},
+  {name= "Pharmacie",color=77, id=51,x = -255.4083, y = 6332.64, z = 32.42725},]]
+
+  --Shop Ottos
+  {name= "Otto Store", id=566,x = 834.0272, y = -812.6409, z = 26.3326},
+  {name= "Otto Store", id=566,x = 108.1267, y = 6628.925, z = 31.7873},
+
+  {name= "Pawn Shop", id=267,x = -273.1787, y = 240.0462, z = 90.57474},
+}
